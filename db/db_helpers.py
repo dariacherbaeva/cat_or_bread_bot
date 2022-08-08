de39bb62c716d30b18306750feca6e7195abc4e6
dariacherbaeva <dashacherbaeva@mail.ru>
2022-08-08 16:16:10 +0300

    Update db helpers

--- db/db_helpers.py
+++ db/db_helpers.py
@@ -4,10 +4,12 @@ def db_add_user(user_id: int, username: str, chat_id: str, conn, cursor):
     conn.commit()
 
 
-def db_add_message(message_text: str, user_id: str, previous_message_text: str, timestamp, conn, cursor):
+def db_add_message(message_text: str, user_id: str, previous_message_text: str,
+                   next_message_text: str, timestamp, conn, cursor):
     cursor.execute(
-        'INSERT INTO message (user_info_id, message_text, timestamp, previous_bot_message_text) VALUES (?, ?, ?, ?)',
-        (user_id, message_text, timestamp, previous_message_text))
+        'INSERT INTO message (user_info_id, message_text, timestamp, previous_bot_message_text,'
+        ' next_bot_message_text) VALUES (?, ?, ?, ?, ?)',
+        (user_id, message_text, timestamp, previous_message_text, next_message_text))
     conn.commit()
 
 
